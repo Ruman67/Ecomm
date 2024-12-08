394ddc696069ca781abf417d51337a2b3164c8dc
Ruman67 <rumanmaharjan888@gmail.com>
2024-12-08 22:06:48 +0545

    Newsletter folder created

---
+++ frontend/src/Components/Newsletter/Newsletter.jsx
@@ -0,0 +1,16 @@
+ import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>Get Exclusive Offers </h1>
+        <p>Subscibe to our newsletter </p>
+    <div>
+        <input type="email" placeholder='Your placeholder id'/>
+        <button>Subscribe</button>
+    </div>
+    </div>
+  )
+}
+
+export default Newsletter
