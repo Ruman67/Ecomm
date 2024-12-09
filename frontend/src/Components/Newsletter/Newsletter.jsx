33de81bc335ce96c0d9f7750f11ec214bbc20496
Ruman67 <rumanmaharjan888@gmail.com>
2024-12-09 15:57:51 +0545

    newsletter button fixed

--- frontend/src/Components/Newsletter/Newsletter.jsx
+++ frontend/src/Components/Newsletter/Newsletter.jsx
@@ -1,16 +1,19 @@
- import React from 'react'
+import React from 'react';
+import './Newsletter.css';
 
 const Newsletter = () => {
   return (
-    <div className='newsletter'>
-        <h1>Get Exclusive Offers </h1>
-        <p>Subscibe to our newsletter </p>
-    <div>
-        <input type="email" placeholder='Your placeholder id'/>
-        <button>Subscribe</button>
+    <div className="newsletter">
+      <div className="newsletter-content">
+        <h1>Get Exclusive Offers on Your Email</h1>
+        <p>Subscribe to our newsletter and stay updated</p>
+        <div className="newsletter-form">
+          <input type="email" placeholder="Your email id" />
+          <button>Subscribe</button>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
